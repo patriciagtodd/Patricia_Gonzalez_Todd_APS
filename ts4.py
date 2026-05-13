ae3ac3560580f70e7453f144c8a8272b05e8ae89
patriciagtodd <pgonzaleztodd@estudiantes.unsam.edu.ar>
2026-05-13 20:02:12 -0300

    Add files via upload

--- ts4.py
+++ ts4.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-#from scipy import signal
+from scipy.signal import windows
 
 
 def mi_funcion_ruido (N, Pr):
@@ -52,8 +52,8 @@ matriz_Sr = matriz_S + r
 # %% SIN VENTANA
 
 w1 = np.ones(N)
-w1 = w1.reshape(1,N)
-w1 = np.tile(w1, (R,1))
+w1 = w1.reshape(N,1)
+w1 = np.tile(w1, (1,R))
 
 a1 = np.fft.fft(matriz_Sr,n=N,axis=0)/(N)  # normalizada con las muestras
 #n y axis lo que hacen es decirle en que direccion y como es cada grupo que quiero analizar
@@ -65,38 +65,35 @@ a1_db = 10*np.log10(a1[:N//2] + 1e-12) #db
 
 ffreq = np.arange(0, N)*deltaf
 
-# Gráfico SOLO DEL DELTA
-#plt.figure(figsize=(10, 10))
-plt.plot(a1_db) 
 
-# línea vertical en x = 3
+plt.plot(a1_db) 
 
 plt.xlim(0, fs/2)
 plt.legend()
 plt.grid(0.01)
 plt.show()
 '''HASTA ACA SE QUE ESTOY ENTRE -2 Y 2'''
+
+
 # %% VENTANA FLATTOP
 
-# %%
+w2 = windows.flattop(N)
+w2 = w2.reshape(N,1)
+w2 = np.tile(w2, (1,R))
 
-w1 = (N)
-w1 = w1.reshape(1,N)
-w1 = np.tile(w1, (R,1))
-
-a1 = np.fft.fft(matriz_S,n=N,axis=0)/(N)  # normalizada con las muestras
+a2 = np.fft.fft((matriz_Sr*w2),n=N,axis=0)/(N)  # normalizada con las muestras
 #n y axis lo que hacen es decirle en que direccion y como es cada grupo que quiero analizar
-a1 = (np.abs(a1)**2)*2 
+a2 = (np.abs(a2)**2)*2 
 #me queda el espectro de un solo lado porque antes se distribuia 1/2 a cada lado
 
 # paso a db
-a1_db = 10*np.log10(a1[:N//2] + 1e-12) #db
+a2_db = 10*np.log10(a2[:N//2] + 1e-12) #db
 
 ffreq = np.arange(0, N)*deltaf
 
 # Gráfico SOLO DEL DELTA
 #plt.figure(figsize=(10, 10))
-plt.plot(a1_db) 
+plt.plot(a2_db) 
 
 # línea vertical en x = 3
 
@@ -108,14 +105,40 @@ plt.show()
 
 
 
+# %% VENTANA Blackmanharris
 
+w3 = windows.blackmanharris(N)
+w3 = w3.reshape(N,1)
+w3 = np.tile(w3, (1,R))
+
+a3 = np.fft.fft((matriz_Sr*w3),n=N,axis=0)/(N)  # normalizada con las muestras
+#n y axis lo que hacen es decirle en que direccion y como es cada grupo que quiero analizar
+a3 = (np.abs(a3)**2)*2 
+#me queda el espectro de un solo lado porque antes se distribuia 1/2 a cada lado
+
+# paso a db
+a3_db = 10*np.log10(a3[:N//2] + 1e-12) #db
+
+ffreq = np.arange(0, N)*deltaf
+
+# Gráfico SOLO DEL DELTA
+#plt.figure(figsize=(10, 10))
+plt.plot(a3_db) 
+
+# línea vertical en x = 3
+
+plt.xlim(0, fs/2)
+plt.legend()
+plt.grid(0.01)
+plt.show()
+'''HASTA ACA SE QUE ESTOY ENTRE -2 Y 2'''
 
 # %% Estimadores
 
-
 #para mi primer estimador lo que quiero es ver que hay en la feta de omega 0
-estimador1_a1 = a1_db[int(N/4), :]
-
+estimador1_a1 = a1[int(N/4), :]
+estimador1_a2 = a2[int(N/4), :]
+estimador1_a3 = a3[int(N/4), :]
 
 #quiero llegar a una matriz de rx4 de ahi hago el histograma, ese 4 son las 4 ventanas que quiero probar
 
@@ -127,4 +150,28 @@ hay lit una funcion que es argmax  y ese va a ser mi estimador (omega sombrerito
  luego el sesgo es omega sombrerito menos omega
  quiero detectar esos maximos y por si acaso verificar que  esten bien
 '''
+estimador2_a1 = np.argmax(a1, axis = 0)
+estimador2_a2 = np.argmax(a2, axis = 0)
+estimador2_a3 = np.argmax(a3, axis = 0)
+
+Mu_a = np.mean(a1)
+
+sesgo = Mu_a - a0
+
+#%% Hitograma amplitud 
+plt.hist(estimador1_a1, bins=15, density=True, alpha=0.7, edgecolor='black', label = "Estimador 1 ventana unitaria")
+plt.hist(estimador1_a2, bins=15, density=True, alpha=0.7, edgecolor='black', label = "Estimador 1 ventana Flattop")
+plt.hist(estimador1_a3, bins=15, density=True, alpha=0.7, edgecolor='black', label = "Estimador 1 ventana Blackmanharris")
+
+# Hitograma arg 
+plt.hist(estimador2_a1, bins=15, density=True, alpha=0.7, edgecolor='black', label = "Estimador 2 ventana unitaria")
+plt.hist(estimador2_a2, bins=15, density=True, alpha=0.7, edgecolor='black', label = "Estimador 2 ventana Flattop")
+plt.hist(estimador2_a3, bins=15, density=True, alpha=0.7, edgecolor='black', label = "Estimador 2 ventana Blackmanharris")
+
+plt.grid(True)
+plt.legend()
+plt.show()
+
+
+
 
